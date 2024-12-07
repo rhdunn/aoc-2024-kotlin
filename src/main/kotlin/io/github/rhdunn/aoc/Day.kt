ec732b0c78e16de2a14f579cb980509294343df1
Reece H. Dunn <msclrhd@gmail.com>
2024-12-07 13:09:52 +0000

    Calculate the time it takes to find an answer.

--- src/main/kotlin/io/github/rhdunn/aoc/Day.kt
+++ src/main/kotlin/io/github/rhdunn/aoc/Day.kt
@@ -1,6 +1,8 @@
 // Copyright (C) 2024 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package io.github.rhdunn.aoc
 
+import kotlin.time.Duration.Companion.milliseconds
+
 abstract class Day<T>(val day: Int) {
     abstract fun part1(data: String): T
     abstract fun part2(data: String): T
@@ -17,7 +19,12 @@ fun List<Day<*>>.run(dayPart: String, data: String) {
         val day = dayPart.substringBefore('-').toIntOrNull()
             ?: throw IllegalArgumentException("unknown day-part: $dayPart")
         val handler = find { it.day == day } ?: throw IllegalArgumentException("unknown day: $dayPart")
-        println(handler.run(data, dayPart))
+
+        val startTime = System.currentTimeMillis()
+        val result = handler.run(data, dayPart)
+        val duration = (System.currentTimeMillis() - startTime).milliseconds
+
+        println("answer $result took $duration to solve")
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
